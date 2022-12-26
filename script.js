a4fc3c81d1de8de4209dff01e5fbab589081e483
Rumen Rashev <57558028+rumenrashev@users.noreply.github.com>
2022-12-26 14:38:05 +0200

    add logic

---
+++ script.js
@@ -0,0 +1,148 @@
+function createEmptyBoard(){
+    return[
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+    ];
+}
+
+let board = createEmptyBoard();
+
+function enter(id){
+    const [rowIndex,colIndex] = id.split('-');
+    const value = Number(document.getElementById(id).value);
+    if(isValid(board,rowIndex,colIndex,value)){
+        board[rowIndex][colIndex] = value;
+    }else if(!isValidRow(board,rowIndex,value)){
+        alert(`'${value}' alredy exists in the row.`)
+    }else if(!isValidCol(board,colIndex,value)){
+        alert(`'${value}' alredy exists in the col.`)
+    }
+    else if(!isValidSquare(board,rowIndex,colIndex,value)){
+        alert(`'${value}' alredy exists in the inner square.`)
+    }
+}
+
+
+function createInput(rowIndex,colIndex){
+    const input = document.createElement('input');
+    const value = board[rowIndex][colIndex];
+    input.id = rowIndex + '-' + colIndex;
+    if(value != 0){
+        input.value = value;
+    }
+    input.type = 'number';
+    input.min = 1;
+    input.max = 9;
+    input.setAttribute('oninput',`enter("${input.id}")`);
+    input.setAttribute('onfocus',"this.value=''");
+    input.setAttribute('autocomplete',"off");
+    return input;
+}
+
+
+
+
+
+function display(){
+
+    document.querySelector('table').innerHTML = '';
+
+    for(let rowIndex = 0; rowIndex < 9; rowIndex++){
+        const tableRow = document.createElement('tr');
+        for(let colIndex = 0; colIndex < 9; colIndex++){
+            const tableCol = document.createElement('td');
+            const input = createInput(rowIndex,colIndex);
+            tableCol.appendChild(input);
+            tableRow.appendChild(tableCol);
+        }
+        document.querySelector('table').appendChild(tableRow);
+    }
+}
+
+
+display();
+
+
+
+function isValidSquare(board, row, col, value) {
+    localRow = row - (row % 3);
+    localCol = col - (col % 3);
+    for (let i = localRow; i < localRow + 3; i++) {
+        for (let j = localCol; j < localCol + 3; j++) {
+            if (board[i][j] == value) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function isValidCol(board, col, value) {
+    for (let row = 0; row < 9; row++) {
+        if (board[row][col] == value) {
+            return false;
+        }
+    }
+    return true
+}
+
+function isValidRow(board, row, value) {
+    for (let col = 0; col < 9; col++) {
+        if (board[row][col] == value) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isValid(board, row, col, value) {
+    return isValidRow(board, row, value) 
+    && isValidCol(board, col, value) 
+    && isValidSquare(board, row, col, value);
+}
+
+function solveBoard(){
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (board[row][col] == 0) {
+                for (let value = 1; value <= 9; value++) {
+                    if (isValid(board, row, col, value)) {
+                        board[row][col] = value;
+                        if (solveBoard()) {
+                            return true;
+                        } else {
+                            board[row][col] = 0;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+
+function solve(){
+    solveBoard();
+    display();
+    document.querySelectorAll('input').forEach(input=> input.disabled = true);
+}
+
+
+
+function reset(){
+    board = createEmptyBoard();
+    display();
+}
+
+
+
